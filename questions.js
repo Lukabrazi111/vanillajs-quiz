10c7a8774ded2992754071f6e92918c314f7d4c9
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2022-07-25 21:49:34 +0400

    style: quiz app

---
+++ questions.js
@@ -0,0 +1,23 @@
+const questions = [
+    {
+        question: 'What is 2 + 2?',
+        answers: [
+            {text: '2', correct: true},
+            {text: '4', correct: false},
+        ]
+    },
+    {
+        question: 'Who is George Hotz?',
+        answers: [
+            {text: 'Programmer', correct: true},
+            {text: 'Mechanic', correct: false},
+        ]
+    },
+    {
+        question: 'What is the main city in Georgia?',
+        answers: [
+            {text: 'Tbilisi', correct: true},
+            {text: 'Batumi', correct: false},
+        ]
+    },
+];
